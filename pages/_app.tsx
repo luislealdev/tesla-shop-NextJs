72312ee4e45911995e1be11e30820b0307625d88
Luis Leal <luisrrleal@gmail.com>
2023-01-15 19:38:00 -0600

    Add material ui and files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material';
 import type { AppProps } from 'next/app'
+import { lightTheme } from '../themes/light-theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
