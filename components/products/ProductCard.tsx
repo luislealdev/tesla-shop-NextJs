e3d259d3388761d50138f6e1ba3d38e82850bdbd
Luis Leal <luisrrleal@gmail.com>
2023-01-21 19:55:19 -0600

    Show text when image is loaded

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -11,40 +11,42 @@ interface Props {
 export const ProductCard: FC<Props> = ({ product }) => {
 
     const [isHovered, setIsHovered] = useState(false);
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
 
     const productImage = useMemo(() => {
         return isHovered
-          ? `products/${ product.images[1] }`
-          : `products/${ product.images[0] }`;
+            ? `products/${product.images[1]}`
+            : `products/${product.images[0]}`;
 
     }, [isHovered, product.images])
 
     return (
-      <Grid item 
-            xs={6} 
-            sm={ 4 }
-            onMouseEnter={ () => setIsHovered(true) } 
-            onMouseLeave={ () => setIsHovered(false) } 
-      >
-          <Card>
-              <NextLink href="/product/slug" passHref prefetch={ false }>
+        <Grid item
+            xs={6}
+            sm={4}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <Card>
+                <NextLink href="/product/slug" passHref prefetch={false}>
                     <CardActionArea>
-                        <CardMedia 
+                        <CardMedia
                             component='img'
                             className='fadeIn'
-                            image={ productImage }
-                            alt={ product.title }
+                            image={productImage}
+                            alt={product.title}
+                            onLoad={() => setIsImageLoaded(true)}
                         />
 
                     </CardActionArea>
-              </NextLink>
-              
-          </Card>
+                </NextLink>
 
-          <Box sx={{ mt: 1 }} className='fadeIn'>
-              <Typography fontWeight={700}>{ product.title }</Typography>
-              <Typography fontWeight={500}>{ `$${product.price}` }</Typography>
-          </Box>
-      </Grid>
+            </Card>
+
+            <Box sx={{ mt: 1, display: isImageLoaded ? 'block' : 'none' }} className='fadeIn'>
+                <Typography fontWeight={700}>{product.title}</Typography>
+                <Typography fontWeight={500}>{`$${product.price}`}</Typography>
+            </Box>
+        </Grid>
     )
 }
