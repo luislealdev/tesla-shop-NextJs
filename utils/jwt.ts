a67d22d492289db0504af4096bc3c83f6266b16e
Luis Leal <luisrrleal@gmail.com>
2023-01-26 12:34:15 -0600

    Faster jwt validation

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -18,6 +18,8 @@ export const signToken = (_id: string, email: string) => {
 export const isValidToken = (token: string): Promise<string> => {
     if (!process.env.JWT_SECRET_SEED) throw new Error('No hay semilla de JWT, revisar variables de entorno');
 
+    if (token.length < 10) return Promise.reject('JWT no es válido');
+
     return new Promise((resolve, reject) => {
         try {
             jwt.verify(token, process.env.JWT_SECRET_SEED || '', (err, payload) => {
