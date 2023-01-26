edf22069d96c93dd00235d9fccfa3f7cb44a9ac0
Luis Leal <luisrrleal@gmail.com>
2023-01-25 21:01:18 -0600

    Validate and renew JWT and add react hook form

---
+++ pages/api/user/validate-token.ts
@@ -0,0 +1,54 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '@/database';
+import User from '../../../models/User';
+import { jwt } from '@/utils';
+
+type Data =
+    | { message: string }
+    | {
+        token: string,
+        user: {
+            email: string,
+            role: string,
+            name: string
+        }
+    }
+
+export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
+    switch (req.method) {
+        case 'GET':
+            return validateToken(req, res);
+        default:
+            return res.status(400).json({ message: 'Bad Request' })
+    }
+}
+
+const validateToken = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+    
+    const { token = '' } = req.cookies;
+
+    let userId = '';
+
+    try {
+        userId = await jwt.isValidToken(token);
+    } catch (error) {
+        return res.status(401).json({ message: 'Auth token not valid' })
+    }
+
+    await db.connect();
+    const user = await User.findById(userId);
+    await db.disconnect();
+    if (!user) return res.status(400).json({ message: 'User with that id doesnt exists' });
+
+    const { _id, email, role, name } = user;
+
+    res.status(200).json({
+        token: jwt.signToken(_id, email),
+        user: {
+            email,
+            role,
+            name
+        }
+    })
+}
--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,40 +1,52 @@
 import NextLink from 'next/link';
-import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { Box, Button, Grid, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../../components/layouts'
+import { useForm } from 'react-hook-form';
+
+type Inputs = {
+    email: string,
+    password: string,
+};
+
+const onSubmit = (data: Inputs) => {
+    console.log({ data });
+}
 
 const LoginPage = () => {
-  return (
-    <AuthLayout title={'Ingresar'}>
-        <Box sx={{ width: 350, padding:'10px 20px' }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <Typography variant='h1' component="h1">Iniciar Sesión</Typography>
-                </Grid>
+    const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
 
-                <Grid item xs={12}>
-                    <TextField label="Correo" variant="filled" fullWidth />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField label="Contraseña" type='password' variant="filled" fullWidth />
-                </Grid>
+    return (
+        <AuthLayout title={'Ingresar'}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Box sx={{ width: 350, padding: '10px 20px' }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Typography variant='h1' component="h1">Iniciar Sesión</Typography>
+                        </Grid>
 
-                <Grid item xs={12}>
-                    <Button color="secondary" className='circular-btn' size='large' fullWidth>
-                        Ingresar
-                    </Button>
-                </Grid>
+                        <Grid item xs={12}>
+                            <TextField label="Correo" variant="filled" fullWidth type='email' {...register('email')} />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField label="Contraseña" type='password' variant="filled" fullWidth {...register('password')} />
+                        </Grid>
 
-                <Grid item xs={12} display='flex' justifyContent='end'>
-                    <NextLink href="/auth/register" passHref>
-                        <Link underline='always'>
-                            ¿No tienes cuenta?
-                        </Link>
-                    </NextLink>
-                </Grid>
-            </Grid>
-        </Box>
-    </AuthLayout>
-  )
+                        <Grid item xs={12}>
+                            <Button color="secondary" className='circular-btn' size='large' fullWidth type='submit'>
+                                Ingresar
+                            </Button>
+                        </Grid>
+
+                        <Grid item xs={12} display='flex' justifyContent='end'>
+                            <NextLink href="/auth/register" passHref style={{ textDecoration: 'underline' }}>
+                                ¿No tienes cuenta?
+                            </NextLink>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </form>
+        </AuthLayout>
+    )
 }
 
 export default LoginPage
