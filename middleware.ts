5998097528c369dec64021e4b3212e7f3f047953
Luis Leal <luisrrleal@gmail.com>
2023-01-27 16:49:24 -0600

    Middleware with next 13

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,43 @@
- // middleware.ts
- import { NextResponse, type NextRequest } from 'next/server'
- 
- import * as jose from 'jose';
-  
- export async function middleware(req: NextRequest) {
-   const previousPage = req.nextUrl.pathname;
-  
-   if (previousPage.startsWith('/checkout')) {
-     const token = req.cookies.get('token')?.value || '';
-  
-     try {
-       await jose.jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET_SEED));
-       return NextResponse.next();
-     } catch (error) {
-       return NextResponse.redirect(
-         new URL(`/auth/login?p=${previousPage}`, req.url)
-       );
-     }
-   }
- };
-  
- export const config = {
-   matcher: [
-     '/checkout/:path*'
-   ],
- };
+// middleware.ts
+import { getToken } from 'next-auth/jwt';
+import { NextResponse, type NextRequest } from 'next/server'
+
+//  import * as jose from 'jose';
+
+export async function middleware(req: NextRequest) {
+
+
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (!session) {
+    const previousPage = req.nextUrl.pathname;
+    const url = req.nextUrl.clone();
+    url.pathname = `/auth/login`;
+    url.search = `p=${previousPage}`;
+
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+
+
+  //  WITH OUR CUSTOM AUTH
+  //  if (previousPage.startsWith('/checkout')) {
+  //    const token = req.cookies.get('token')?.value || '';
+
+  //    try {
+  //      await jose.jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET_SEED));
+  //      return NextResponse.next();
+  //    } catch (error) {
+  //      return NextResponse.redirect(
+  //        new URL(`/auth/login?p=${previousPage}`, req.url)
+  //      );
+  //    }
+  //  }
+};
+
+export const config = {
+  matcher: [
+    '/checkout/:path*'
+  ],
+};
