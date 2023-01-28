1839307cf82315df64496b41f2195deae61b6105
Luis Leal <luisrrleal@gmail.com>
2023-01-27 20:48:12 -0600

    Create backend p1

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,4 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { IOrder } from '../../../interfaces/order';
+import { getSession } from 'next-auth/react';
+import { db, dbProducts } from '@/database';
+import Product from '../../../models/Product';
 
 type Data = {
     message: string
@@ -14,9 +18,28 @@ export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
     }
 }
 
-const createNewOrder = (req: NextApiRequest, res: NextApiResponse) => {
-    const orderBody = req.body;
-    console.log(orderBody);
+const createNewOrder = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { orderItems, total } = req.body as IOrder;
+    const session: any = await getSession();
+
+    if (!session) return res.status(400).json({ message: 'Debe estar autenticado para hacer esto' });
+
+    //Create array with the products the person wants
+    const productsId = orderItems.map(product => product._id);
+    await db.connect();
+
+    const dbProducts = await Product.find({ _id: { $in: productsId } });
+    try {
+        const subTotal = orderItems.reduce((prev, current) => {
+            const currentPrice = dbProducts.find(prod => prod._id === current._id)?.price;
+            if (!currentPrice) throw new Error("Verifique el carrito de nuevo, producto inexistente.");
+
+            return (current.price * current.quantity) + prev;
+        }, 0);
+    } catch (error) {
+
+    }
+
 
     return res.status(200).json({ message: 'Created' })
 }
