40e6dbb2677b4bf87eb22fb63b570b25fe558788
Luis Leal <luisrrleal@gmail.com>
2023-01-31 21:15:52 -0600

    Fix bugs

--- pages/api/admin/dashboard.ts
+++ pages/api/admin/dashboard.ts
@@ -1,23 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import Order from '../../../models/Order';
-import User from '../../../models/User';
-import Product from '../../../models/Product';
-import { db } from '@/database';
+import { db } from '../../../database'
+import { Order, Product, User } from '../../../models';
 
 type Data = {
-    numberOfItems: number,
-    paidOrders: number,
-    numberOfClients: number,
-    numberOfProducts: number,
-    productsWithNoInventory: number,
-    lowInventory: number
+    numberOfOrders: number;
+    paidOrders: number; // isPad true
+    notPaidOrders: number;
+    numberOfClients: number; // role: client
+    numberOfProducts: number;
+    productsWithNoInventory: number; // 0
+    lowInventory: number; // productos con 10 o menos
 }
 
-export default async function (req: NextApiRequest, res: NextApiResponse<Data>) {
-    await db.connect();
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 
+    await db.connect();
+    
+    // const numberOfOrders = await Order.count();
+    // const paidOrders = await Order.find({ isPaid: true }).count();
+    // const numberOfClients = await User.find({ role: 'client' }).count();
+    // const numberOfProducts = await Product.count();
+    // const productsWithNoInventory = await Product.find({ inStock: 0 }).count();
+    // const lowInventory = await Product.find({ inStock: { $lte: 10 } }).count();
+    
     const [
-        numberOfItems,
+        numberOfOrders,
         paidOrders,
         numberOfClients,
         numberOfProducts,
@@ -29,16 +36,21 @@ export default async function (req: NextApiRequest, res: NextApiResponse<Data>) 
         User.find({ role: 'client' }).count(),
         Product.count(),
         Product.find({ inStock: 0 }).count(),
-        Product.find({ inStock: { $lte: 10 } }).count()
-    ]
-    )
+        Product.find({ inStock: { $lte: 10 } }).count(),
+    ]);
+
+    
     await db.disconnect();
+
     res.status(200).json({
-        numberOfItems,
+        numberOfOrders,
         paidOrders,
         numberOfClients,
         numberOfProducts,
         productsWithNoInventory,
         lowInventory,
+        notPaidOrders: numberOfOrders - paidOrders
     })
+
+
 }
