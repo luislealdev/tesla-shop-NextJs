c75428c221708a74577b0b834471a9c2c824c4eb
Luis Leal <luisrrleal@gmail.com>
2023-01-31 21:42:28 -0600

    Middlewares to validate auth

--- middleware.ts
+++ middleware.ts
@@ -2,42 +2,48 @@
 import { getToken } from 'next-auth/jwt';
 import { NextResponse, type NextRequest } from 'next/server'
 
-//  import * as jose from 'jose';
-
 export async function middleware(req: NextRequest) {
-
-
-  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const session: any = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
   if (!session) {
+    if (req.nextUrl.pathname.startsWith('/api/admin')) {
+      return new Response(JSON.stringify({ message: 'No autorizado' }), {
+        status: 401,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    }
+
     const previousPage = req.nextUrl.pathname;
     const url = req.nextUrl.clone();
     url.pathname = `/auth/login`;
     url.search = `p=${previousPage}`;
-
     return NextResponse.redirect(url);
   }
 
+  if (req.nextUrl.pathname.startsWith('/admin')) {
+    const validRoles = ['admin', 'super-user', 'SEO'];
+    if (!validRoles.includes(session.user.role)) {
+      return NextResponse.redirect(new URL('/', req.url));
+    }
+  }
+
+  if (req.nextUrl.pathname.startsWith('/api/admin')) {
+    const validRoles = ['admin', 'super-user', 'SEO'];
+    if (!validRoles.includes(session.user.role)) {
+      return new Response(JSON.stringify({ message: 'No autorizado' }), {
+        status: 401,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    }
+  }
+
   return NextResponse.next();
-
-
-  //  WITH OUR CUSTOM AUTH
-  //  if (previousPage.startsWith('/checkout')) {
-  //    const token = req.cookies.get('token')?.value || '';
-
-  //    try {
-  //      await jose.jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET_SEED));
-  //      return NextResponse.next();
-  //    } catch (error) {
-  //      return NextResponse.redirect(
-  //        new URL(`/auth/login?p=${previousPage}`, req.url)
-  //      );
-  //    }
-  //  }
 };
 
 export const config = {
-  matcher: [
-    '/checkout/address', '/checkout/summary'
-  ],
+  matcher: ['/checkout/:path*', '/admin/:path*', '/api/admin/:path*'],
 };
